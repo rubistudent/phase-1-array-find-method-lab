f118106fc2a46466c9da68b07cdf187c97461626
www.rubiluqman7@gmail.com <www.rubiluqman7@gmail.com>
2022-09-08 23:35:56 +0300

    complites assinment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(record) {
+	let obj = record.find(function (obj) {
+		return obj.result === "W";
+	});
+	if (typeof obj === "undefined") {
+		return undefined;
+	} else {
+		return obj.year;
+	}
+}
